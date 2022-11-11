f9b613b2166e5138d5d3238348310a99c948f8da
BrunoFranz31 <brunofranz2006@gmail.com>
2022-11-11 19:08:20 -0300

    commitando

---
+++ wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(
+    function () {
+        $("#frmCadastro").submit(
+            function (e) {
+                e.preventDefault();
+                Cadastrar();
+            }
+        );
+    }
+
+);
+
+function Cadastrar() {
+    let parametros = {
+        Nome: $("#nome").val(),
+        Email: $("#email").val(),
+        Mensagem: $("#msg").val()
+    };
+    $.post("/Home/Cadastrar", parametros).done(
+        function (data) {
+            if (data.status == "OK") {
+                $("#frmCadastro").after("<h3>Cadastro enviado com sucesso!</h3>")
+                $("#frmCadastro").hide();
+            }
+            else {
+                alert(data.mensagem);
+            }
+        }).fail(function () {
+            alert("Falha no cadastro, tente novamente!");
+        })
+};
